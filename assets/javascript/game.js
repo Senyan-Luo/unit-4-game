285640ef7c96de0ed78c76e46156e61d944d3090
Charlotte Luo <sel152@ucsd.edu>
2018-06-09 19:52:41 -0700

    finised

---
+++ assets/javascript/game.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+    var scoreSum = 0
+    var numWins = 0
+    var numLosses = 0
+
+   
+    $("#numWins").text('Wins: ' + numWins)
+    $("#numLosses").text('Losses '+ numLosses)
+    $("#scoreSum").text('Currently at: '+ scoreSum)
+
+        var goalNumber= Math.floor(Math.random() * 102 + 19); 
+        $("#goalNumber").text('Goal Number: '+ goalNumber)
+
+        var val1 = Math.floor(Math.random() * 12  + 1)
+        var val2 = Math.floor(Math.random() * 12  + 1)
+        var val3 = Math.floor(Math.random() * 12  + 1)
+        var val4 = Math.floor(Math.random() * 12  + 1)
+
+    $("#dog1").on("click", function(){
+         scoreSum = scoreSum + val1
+         $("#scoreSum").text('Currently at: '+ scoreSum)
+         if(scoreSum === goalNumber){
+             win()
+         }
+         else if(scoreSum > goalNumber){
+             lose()
+         }
+    })
+    $("#dog2").on("click", function(){
+        scoreSum += val2
+        $("#scoreSum").text('Currently at: '+ scoreSum)
+         if(scoreSum === goalNumber){
+             win()
+         }
+         else if(scoreSum > goalNumber){
+             lose()
+         }
+    })
+    $("#dog3").on("click", function(){
+        scoreSum += val3
+        $("#scoreSum").text('Currently at: '+ scoreSum)
+         if(scoreSum === goalNumber){
+             win()
+         }
+         else if(scoreSum > goalNumber){
+             lose()
+         }
+    })
+    $("#dog4").on("click", function(){
+        scoreSum += val4
+        $("#scoreSum").text('Currently at: '+ scoreSum)
+         if(scoreSum === goalNumber){
+             win()
+         }
+         else if(scoreSum > goalNumber){
+             lose()
+         }
+    })
+    function win(){
+        numWins ++
+        reset()
+        $("#numWins").text('Wins: ' + numWins)
+        
+    }
+    function lose(){
+        numLosses ++
+        reset()
+         $("#numLosses").text('Losses '+ numLosses)
+
+    }
+    function reset(){
+        var goalNumber= Math.floor(Math.random() * 102 + 19); 
+        $("#goalNumber").text('Goal Number: '+ goalNumber)
+        var val1 = Math.floor(Math.random() * 12  + 1)
+        var val2 = Math.floor(Math.random() * 12  + 1)
+        var val3 = Math.floor(Math.random() * 12  + 1)
+        var val4 = Math.floor(Math.random() * 12  + 1)
+        scoreSum = 0
+        $("#scoreSum").text('Currently at: '+ scoreSum)
+    }
+})
